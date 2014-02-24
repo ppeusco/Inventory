04addabb008f534a1a03d3fe63334abd7a130da8
Pablo Peuscovich <pablo.peuscovich@globant.com>
2014-02-24 08:57:45 -0300

    Authenticate-users-finished

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -11,8 +11,6 @@ $this->breadcrumbs=array(
 
 <h1>Login</h1>
 
-<p>Please fill out the following form with your login credentials:</p>
-
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'login-form',
@@ -22,32 +20,26 @@ $this->breadcrumbs=array(
 	),
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+    <div class="control-group">
+        <?php echo $form->labelEx($model, 'username', array('class' => 'control-label')); ?>
+        <div class="controls">
+            <?php echo $form->textField($model, 'username'); ?>
+            <?php echo $form->error($model, 'username'); ?>
+        </div>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username'); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
+    <div class="control-group">
+        <?php echo $form->labelEx($model, 'password', array('class' => 'control-label')); ?>
+        <div class="controls">
+            <?php echo $form->passwordField($model, 'password'); ?>
+            <?php echo $form->error($model, 'password'); ?>
+        </div>
+    </div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'password'); ?>
-		<?php echo $form->passwordField($model,'password'); ?>
-		<?php echo $form->error($model,'password'); ?>
-		<p class="hint">
-			Hint: You may login with <kbd>demo</kbd>/<kbd>demo</kbd> or <kbd>admin</kbd>/<kbd>admin</kbd>.
-		</p>
-	</div>
-
-	<div class="row rememberMe">
-		<?php echo $form->checkBox($model,'rememberMe'); ?>
-		<?php echo $form->label($model,'rememberMe'); ?>
-		<?php echo $form->error($model,'rememberMe'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Login'); ?>
-	</div>
-
+    <div class="control-group">
+        <div class="controls">
+            <?php echo CHtml::submitButton('Login', array('class' => 'btn btn-primary')); ?>
+        </div>
+    </div>
 <?php $this->endWidget(); ?>
 </div><!-- form -->
